99192135b8f2b64611b26dfba0aabf41a4ed8c66
Nicole <nicole.georgievva@gmail.com>
2022-11-29 17:49:12 +0200

    Create a customizable input field composable function: WIP

--- app/src/main/java/com/example/tipcalculator/components/Components.kt
+++ app/src/main/java/com/example/tipcalculator/components/Components.kt
@@ -1,8 +1,13 @@
 package com.example.tipcalculator.components
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun InputField(
@@ -10,7 +15,14 @@ fun InputField(
     valueState: MutableState<String>,
     labelId: String,
     enabled: Boolean,
-    isSingleLine: Boolean
+    isSingleLine: Boolean,
+    keyboardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
 ) {
-
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = labelId) }
+    )
 }
