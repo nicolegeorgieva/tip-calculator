59cf51b7d7784fd54240391445fe20b86a51a5d9
Nicole <nicole.georgievva@gmail.com>
2022-12-26 14:24:52 +0200

    Add a dependency for material icons

--- app/src/main/java/com/example/tipcalculator/components/Components.kt
+++ app/src/main/java/com/example/tipcalculator/components/Components.kt
@@ -1,8 +1,11 @@
 package com.example.tipcalculator.components
 
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -23,6 +26,12 @@ fun InputField(
     OutlinedTextField(
         value = valueState.value,
         onValueChange = { valueState.value = it },
-        label = { Text(text = labelId) }
+        label = { Text(text = labelId) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Refresh,
+                contentDescription = "Money Icon"
+            )
+        }
     )
 }
