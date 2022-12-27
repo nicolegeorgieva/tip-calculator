03dcf9899a5323f68d8452deb38627824ca76624
Nicole <nicole.georgievva@gmail.com>
2022-12-27 13:18:51 +0200

    Fix the input field width

--- app/src/main/java/com/example/tipcalculator/components/Components.kt
+++ app/src/main/java/com/example/tipcalculator/components/Components.kt
@@ -1,5 +1,6 @@
 package com.example.tipcalculator.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -48,7 +49,8 @@ fun InputField(
             color = MaterialTheme.colors.onBackground
         ),
         modifier = modifier
-            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
         enabled = enabled,
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType,
